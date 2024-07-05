82d2ea2cf19aea914da1b96d74c83016fb992ee7
NIcholasWCK <nicholaswong357@gmail.com>
2024-07-05 14:23:30 +0800

    Add files via upload

---
+++ src/components/Sections.vue
@@ -0,0 +1,52 @@
+<script setup>
+</script>
+
+<template>
+  <div class="">
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E0FFFF">
+      <q-img
+        src="~assets/1.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-prima">My sisters</h4>
+        <h6 class="q-px-lg">I have two sisters.they are the cutest,smartestand the most beautiful.I like them.</h6>
+      </div>
+    </div>
+
+
+    <div class="row reverse q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #FFE5E0">
+      <q-img
+        src="~assets/2.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My favorite sport</h4>
+        <h6 class="q-px-lg">I like do more sports because it's good for our health and make our body stronger.For example:playing Basketball and badminton.😄</h6>
+      </div>
+    </div>
+
+
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E6E0FF">
+      <q-img
+        src="~assets/3.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My favorite role</h4>
+        <h6 class="q-px-lg">My favorite role is Marval's Hulk!He is the strongest man I've ever seen!
+
+        </h6>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
